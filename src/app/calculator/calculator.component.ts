dce674db09db0255492095fa73d5c0a069b7b295
b-vivien <134312026+b-viv@users.noreply.github.com>
2024-06-01 17:34:47 +0200

    update setTimeout

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -39,12 +39,9 @@ export class CalculatorComponent {
 
   onSubmit(): void {
     const dayValue: number = this.dateInputForm.value.dayInput;
+    console.log(dayValue);
     const monthValue: number = this.dateInputForm.value.monthInput;
     let yearValue: number = this.dateInputForm.value.yearInput;
-
-    this.yearCounter = 0;
-    this.monthCounter = 0;
-    this.dayCounter = 0;
 
     if (this.dateInputForm.valid && this.isValidLastDayOfMonth(dayValue, monthValue, yearValue)) {
       this.emptyFields = false;
@@ -52,49 +49,75 @@ export class CalculatorComponent {
       this.months = this.currentDate.getMonth() + 1 - monthValue;
       this.days = this.currentDate.getDate() - dayValue;
 
-      setTimeout(() => {
-        let yearCounterStop = setInterval(() => {
-          this.yearCounter++;
-          this.yearDisplay = this.yearCounter.toString();
-          if (this.yearCounter == this.years) {
-            clearInterval(yearCounterStop);
-          }
-        }, 10);
-
-        let monthCounterStop = setInterval(() => {
-          this.monthCounter++;
-          this.monthDisplay = this.monthCounter.toString();
-          if (this.monthCounter == this.months) {
-            clearInterval(monthCounterStop);
-          }
-        }, 10);
-
-        let dayCounterStop = setInterval(() => {
-          this.dayCounter++;
-          this.dayDisplay = this.dayCounter.toString();
-          if (this.dayCounter == this.days) {
-            clearInterval(dayCounterStop);
-          }
-        }, 10);
-      });
-
+      console.log("days: " + this.days);
       if (this.days < 0) {
         this.months--;
         let lastMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 0);
         this.days += lastMonth.getDate();
+        console.log("ha a nap kisebb min 0:" + this.months + " " + this.days);
       }
 
       if (this.months < 0) {
         this.years--;
         this.months += 12;
+        console.log("Ha kisbb mint 0 a hónap" + this.years, this.months);
       }
+
+      setTimeout(() => {
+      this.yearCounter = 0;
+      this.monthCounter = 0;
+      this.dayCounter = 0;
+      console.log("kezdő számláló év hónap nap" + this.yearCounter, this.monthCounter, this.dayCounter);
+
+      let yearCounterStop = setInterval(() => {
+        if (this.years === 0) {
+          this.yearDisplay = this.years.toString();
+          clearInterval(yearCounterStop);
+        }else {
+          this.yearCounter++;
+          this.yearDisplay = this.yearCounter.toString();
+
+          if (this.yearCounter == this.years) {
+            clearInterval(yearCounterStop);
+          }
+        }
+      }, 10);
+
+      let monthCounterStop = setInterval(() => {
+        if (this.months === 0){
+          this.monthDisplay = this.months.toString();
+          clearInterval(monthCounterStop);
+        }else {
+            this.monthCounter++;
+            this.monthDisplay = this.monthCounter.toString();
+
+            if (this.monthCounter == this.months) {
+              clearInterval(monthCounterStop);
+            }
+        }
+      }, 10);
+
+      let dayCounterStop = setInterval(() => {
+        if (this.days === 0){
+          this.dayDisplay = this.days.toString();
+          clearInterval(dayCounterStop);
+        }else {
+            this.dayCounter++;
+            this.dayDisplay = this.dayCounter.toString();
+
+            if (this.dayCounter == this.days) {
+              clearInterval(dayCounterStop);
+            }
+        }
+      }, 10);
+    });
     } else {
       this.emptyFields = true;
       this.years = '--';
       this.months = '--';
       this.days = '--';
     }
-  }
+   }
 
   isValidLastDayOfMonth(day: number, month: number, year: number): boolean {
     let inputLastDayOfMonth = new Date(year, month, 0);
@@ -110,4 +133,3 @@ export class CalculatorComponent {
     }
   }
 }
-
